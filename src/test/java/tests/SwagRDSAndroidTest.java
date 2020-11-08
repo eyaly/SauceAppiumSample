ae77c01ccfe8eefb073a9c5b7f281f328a1802f9
Eyal Yovel <eyalyovel@gmail.com>
2020-11-08 08:51:02 +0000

    Few Appium examples

---
+++ src/test/java/tests/SwagRDSAndroidTest.java
@@ -0,0 +1,130 @@
+package tests;
+
+
+import io.appium.java_client.android.AndroidDriver;
+
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class SwagRDSAndroidTest {
+
+    private static ThreadLocal<AndroidDriver> androidDriver = new ThreadLocal<AndroidDriver>();
+    private  ThreadLocal<String> sessionId = new ThreadLocal<>();
+
+    String usernameID = "test-Username";
+    String passwordID = "test-Password";
+    String submitButtonID = "test-LOGIN";
+    By ProductTitle = By.xpath("//android.widget.TextView[@text='PRODUCTS']");
+
+
+    @BeforeMethod
+    public void setup(Method method) throws MalformedURLException {
+
+        System.out.println("Sauce Android Native - BeforeMethod hook");
+        String username = System.getenv("SAUCE_USERNAME");
+        String accesskey = System.getenv("SAUCE_ACCESS_KEY");
+
+        String sauceUrl = "@ondemand.eu-central-1.saucelabs.com:443";
+        String SAUCE_REMOTE_URL = "https://" + username + ":" + accesskey + sauceUrl +"/wd/hub";
+
+        String methodName = method.getName();
+        URL url = new URL(SAUCE_REMOTE_URL);
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Samsung.*");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("app", "storage:filename=Android.SauceLabs.Mobile.Sample.app.2.3.0.apk");
+        capabilities.setCapability("name", methodName);
+        capabilities.setCapability("appiumVersion", "1.17.0");
+
+      //  capabilities.setCapability("noReset", "true");
+      //  capabilities.setCapability("cacheId", "1234");
+
+        androidDriver.set(new AndroidDriver(url, capabilities));
+
+        String id = ((RemoteWebDriver) getAndroidDriver()).getSessionId().toString();
+        sessionId.set(id);
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult result) {
+        System.out.println("Sauce - AfterMethod hook");
+        try {
+            ((JavascriptExecutor) getAndroidDriver()).executeScript("sauce:job-result=" + (result.isSuccess() ? "passed" : "failed"));
+        } finally {
+            System.out.println("Sauce - release driver");
+            getAndroidDriver().quit();
+        }
+    }
+
+    public  AndroidDriver getAndroidDriver() {
+        return androidDriver.get();
+    }
+
+    @Test
+    public void loginToSwagLabsTestValid() {
+        System.out.println("Sauce - Start loginToSwagLabsTestValid test");
+
+        login("standard_user", "secret_sauce");
+
+        // Verificsation
+        Assert.assertTrue(isOnProductsPage());
+    }
+
+    @Test
+    public void loginToSwagLabsTestValid2() {
+        System.out.println("Sauce - Start loginToSwagLabsTestValid test");
+
+        login("standard_user", "secret_sauce");
+
+        // Verificsation
+        Assert.assertTrue(isOnProductsPage());
+    }
+
+    public void login(String user, String pass){
+        AndroidDriver driver = getAndroidDriver();
+
+        WebElement usernameEdit = (WebElement) driver.findElementByAccessibilityId(usernameID);
+        usernameEdit.click();
+        usernameEdit.sendKeys(user);
+
+        WebElement passwordEdit = (WebElement) driver.findElementByAccessibilityId(passwordID);
+        passwordEdit.click();
+        passwordEdit.sendKeys(pass);
+
+        WebElement submitButton = (WebElement) driver.findElementByAccessibilityId(submitButtonID);
+        submitButton.click();
+    }
+
+    public boolean isOnProductsPage() {
+        AndroidDriver driver = getAndroidDriver();
+        //Create an instance of a Selenium explicit wait so that we can dynamically wait for an element
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        //wait for the product field to be visible and store that element into a variable
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(ProductTitle));
+        } catch (TimeoutException e){
+            return false;
+        }
+        return true;
+    }
+
+}
