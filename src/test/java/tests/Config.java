35cf30e24cca4ab4a2a97d0271e680d57def0193
Eyal Yovel <eyalyovel@gmail.com>
2021-02-22 17:03:29 +0000

    changes in config

--- src/test/java/tests/Config.java
+++ src/test/java/tests/Config.java
@@ -1,7 +1,6 @@
 package tests;
 
 public class Config {
+    public static final String region = System.getProperty("region", "us");
     public static final String host = System.getProperty("host", "saucelabs");
-    public static final String region = System.getProperty("region", "eu");
-
 }
